042955d667216a78ab7a675f033306ca5d8eca1a
shinyaoguri <ogrium@gmail.com>
2020-11-13 19:11:20 +0900

    update

---
+++ mac_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "mac_setup.sh"
--- setup.sh
+++ setup.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
-# ###############################################
-# MIT License
-# Copyright (c) 2020 Shinya Oguri shinyaoguri.com
-# https://github.com/shinyaoguri/dotfiles/blob/main/LICENSE
-# ###############################################
 
-# 参考
-# ミニマルに始めるDotfiles自動化計画 https://qiita.com/okamos/items/40966158d0271ae7198b
-
-has() {
-    type "$1" > /dev/null 2>&1
-}
-
-help() {
-
-}
-
-link_files() {
-
-}
-
-initialize() {
-
-}
+echo "setup.sh"

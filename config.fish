e5589bfa86c5500e7581a81c0aa4430cc425c575
Shinya Oguri <36407060+shinyaoguri@users.noreply.github.com>
2023-02-09 12:27:03 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,28 +2,45 @@
 set -x PATH /opt/homebrew/bin $PATH
 set -x PATH /usr/local/bin $PATH
 
-# deno lume
-if test -d $HOME/.deno
-  set -x PATH ~/.deno/bin $PATH
-end
-
-#エイリアス
+##########
+# エイリアス
+##########
+# rmtでゴミ箱に移動
 alias rmt="trash -F"
+
+# rmは確認させる
 alias rm="rm -i"
 alias g="git"
+
+# fish
 alias relogin="exec fish"
 alias reload="source ~/.config/fish/config.fish"
+
+#
 alias t="tmuximum"
+
+# 打ち間違え対策
 alias '\;s'="ls"
 clear
 
+##########
 # anyenv
+##########
 if test -d $HOME/.anyenv
   set -x PATH $HOME/.anyenv/bin $PATH
   anyenv init - fish | source
 end
 
+##########
+# deno
+if test -d $HOME/.deno
+  set -x PATH ~/.deno/bin $PATH
+end
+##########
+
+##########
 # Android Studio
+##########
 set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
 
 # Fish git prompt
